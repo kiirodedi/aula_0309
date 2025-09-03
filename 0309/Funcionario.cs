2825bbf7e8ab96ee14e27a03870a15ba37ce6bfc
kiirodedi <moraismoralesdeborafernanda3@gmail.com>
2025-09-03 09:53:24 -0300

    meu primeiro commit

---
+++ 0309/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0309
+{
+    internal class Funcionario
+    {
+        private ulong cpf;
+
+        public Funcionario(ulong cpf)
+        { 
+            if (!validarCpf(cpf))
+            {
+                throw new ArgumentException("Cpf inválido - cpf deve conter 11 digitos!");
+            }
+        }
+        private bool validarCpf(ulong cpf)
+        {
+            string _cpf = cpf.ToString();
+            return (_cpf.Length == 11);
+        }
+        public Funcionario() { }
+    }
+}
---
+++ 0309/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0309
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            início:
+            try
+            {
+                Funcionario marcos = new Funcionario();
+                Funcionario maria = new Funcionario(11111111111);
+            }
+            catch ( Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                goto início;
+            }
+            
+           
+
+        }
+    }
+}
